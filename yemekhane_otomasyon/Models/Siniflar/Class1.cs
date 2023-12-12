2ba5d3dfdec7b6e6132b112f69820a5d052a40f9
anacbetul <betul.anacoglu@gmail.com>
2023-12-12 15:04:50 +0300

    class1 eklendi

---
+++ yemekhane_otomasyon/Models/Siniflar/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace yemekhane_otomasyon.Models.Siniflar
+{
+    public class Class1
+    {
+        int a = 5;
+    }
+}
