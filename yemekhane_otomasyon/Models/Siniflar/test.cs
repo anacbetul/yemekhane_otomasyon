2d1e64104cc77fef047c2f3706f1110be357745b
Kadir Capkin <kadircapkinn@hotmail.com>
2023-12-13 15:47:57 +0300

    test  class

---
+++ yemekhane_otomasyon/Models/Siniflar/test.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace yemekhane_otomasyon.Models.Siniflar
+{
+    public class test
+    {
+        // test class
+    }
+}
