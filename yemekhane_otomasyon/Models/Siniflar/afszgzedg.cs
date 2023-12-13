a4da64f6a430617df73880bc36611ad7c5a0828a
anacbetul <betul.anacoglu@gmail.com>
2023-12-13 16:32:50 +0300

    deneme

---
+++ yemekhane_otomasyon/Models/Siniflar/afszgzedg.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace yemekhane_otomasyon.Models.Siniflar
+{
+    public class afszgzedg
+    {
+    }
+}
