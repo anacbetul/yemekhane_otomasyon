7666eda334f364626aa8600700356a6415338d41
anacbetul <betul.anacoglu@gmail.com>
2023-12-13 20:58:04 +0300

    web start

--- yemekhane_otomasyon/Models/Siniflar/Personel.cs
+++ yemekhane_otomasyon/Models/Siniflar/Personel.cs
@@ -4,7 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
-
+    
 namespace yemekhane_otomasyon.Models.Siniflar
 {
     public class Personel
