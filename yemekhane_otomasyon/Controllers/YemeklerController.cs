3e44f03218781d2713e6113fd0547aab2fb615a3
anacbetul <betul.anacoglu@gmail.com>
2023-12-19 16:34:40 +0300

    update

--- yemekhane_otomasyon/Controllers/YemeklerController.cs
+++ yemekhane_otomasyon/Controllers/YemeklerController.cs
@@ -4,17 +4,19 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using yemekhane_otomasyon.Models.Siniflar;
+
 namespace yemekhane_otomasyon.Controllers
 {
     public class YemeklerController : Controller
     {
         // GET: Yemekler
 
-        Context c = new Context();
+        Context a = new Context();
         public ActionResult Index()
         {
-            var urunler = c.Yemeklers.ToList();
+            var urunler = a.Yemeklers.ToList();
             return View(urunler);
         }
+        
     }
 }
