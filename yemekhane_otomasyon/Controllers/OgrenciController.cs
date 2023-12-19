977a72c6ce0f482c81f1a36b9b0c74de5387b798
anacbetul <betul.anacoglu@gmail.com>
2023-12-19 11:56:58 +0300

    arayüz eklemeleri, düzeltmeleri ve cont view ekleme

---
+++ yemekhane_otomasyon/Controllers/OgrenciController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace yemekhane_otomasyon.Controllers
+{
+    public class OgrenciController : Controller
+    {
+        // GET: Ogrenci
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
