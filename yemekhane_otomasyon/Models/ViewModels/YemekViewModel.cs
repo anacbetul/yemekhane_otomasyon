c5782b335033e10eeb00e3f8c35784b762c20ad4
anacbetul <betul.anacoglu@gmail.com>
2023-12-20 16:48:24 +0300

    Menü eklendi

---
+++ yemekhane_otomasyon/Models/ViewModels/YemekViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using yemekhane_otomasyon.Models.Siniflar;
+
+namespace yemekhane_otomasyon.Models.ViewModels
+{
+    public class YemekViewModel
+    {
+        public string GunAdi { get; set; }
+        public List<YemekTur> YemekTurListesi { get; set; }
+        public List<Yemekler> YemeklerListesi { get; set; }
+
+    }
+}
