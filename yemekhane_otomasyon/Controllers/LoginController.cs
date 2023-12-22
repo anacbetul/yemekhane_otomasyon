8b21155413b71247f277f2f2b2ad2a92771b7bf8
Furkan Kazim Cam <furkankazimca@gmail.com>
2023-12-22 11:27:43 +0300

    Öğrenci Login işlemi tamamlandı

--- yemekhane_otomasyon/Controllers/LoginController.cs
+++ yemekhane_otomasyon/Controllers/LoginController.cs
@@ -3,21 +3,42 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
+using yemekhane_otomasyon.Models.Siniflar;
 
 namespace yemekhane_otomasyon.Controllers
 {
     public class LoginController : Controller
     {
         // GET: Login
+        Context c = new Context();
         public ActionResult Index()
         {
             return View();
         }
 
-        public PartialViewResult Partial2()
+        [HttpGet]
+        public ActionResult Ogrencilogin()
         {
             //view eklenecek
-            return PartialView();
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Ogrencilogin(Ogrenci og)
+        {
+            var bilgiler= c.Ogrencis.FirstOrDefault(x => x.OgrenciTc == og.OgrenciTc && x.Şifre == og.Şifre);
+            if(bilgiler != null)
+            {
+                FormsAuthentication.SetAuthCookie(bilgiler.OgrenciTc, false);
+                Session["OgrenciTc"] = bilgiler.OgrenciTc.ToString();
+                return RedirectToAction("Index", "Satis");
+            }
+            else 
+            {
+                return RedirectToAction("Index", "Login");
+            }
+            
         }
     }
 }
